5d1be14864274e9b38dfdc327f2495f382b3ba12
LeaffLovesBingus <mati.sotitol@gmail.com>
2025-05-12 21:00:40 -0400

    fixed categorias

--- relacionalBD/insercionDatosPrueba.py
+++ relacionalBD/insercionDatosPrueba.py
@@ -4,11 +4,11 @@ import json as js_
 import random as rndm
 from faker import Faker
 
-MAX: int= int(10e3)
+MAX: int = int(10e3)
 
 # Llamamos el json con datos para la selección
 
-with open("../categories.json", "r", encoding="utf-8") as f:
+with open("./categorias.json", "r", encoding="utf-8") as f:
     Categorias = js_.load(f)
     
 print(Categorias)
@@ -23,14 +23,17 @@ conn = psycopg2.connect(
 	user="usuario_emprendimiento",
 	password="1234"
 )
-#cur = conn.cursor()
+cur = conn.cursor()
 
 # acá inserto sobre categorías (:v, pero tengo que arreglar lo de arriba)
 # Inserción de datos sobre la tabla "categorias"
-#for i in range(MAX):
+# for i in range(MAX):
 #    cur.execute("INSERT INTO categorias (id_categoria, nombre) VALUES (%s, %s)",
 #                (i+1, "wrof")
 #                )
 #    conn.commit()
 #
 #
+
+cur.close()
+conn.close()
